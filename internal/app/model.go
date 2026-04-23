66a922c99f0d5750a9a3448cb0049fe0632f99ae
TheComputerM <mudit.somani@cockroachlabs.com>
2026-04-23 11:55:57 +0530

    refactor(app): initialize model from state path

--- internal/app/model.go
+++ internal/app/model.go
@@ -1,25 +1,49 @@
 package app
 
 import (
+	"errors"
+	"log"
 	"os"
 	"path/filepath"
 
 	tea "charm.land/bubbletea/v2"
+	"github.com/thecomputerm/lazycph/internal/core"
 	"github.com/thecomputerm/lazycph/internal/screens/filepicker"
 	"github.com/thecomputerm/lazycph/internal/screens/workspace"
 )
 
 type Model struct {
+	// a path that determines the state of the app.
+	// If it is a directory, the file picker is shown.
+	// If it is a file, the workspace is shown.
+	state string
+
 	active tea.Model
 }
 
 var _ tea.Model = (*Model)(nil)
 
-func New() Model {
-	currentDirectory, _ := os.Getwd()
+func New(state string) Model {
+	var active tea.Model
+
+	if info, err := os.Stat(state); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			log.Fatalf("Path %s does not exist", state)
+		}
+		log.Fatalf("Failed to stat path %s: %v", state, err)
+	} else if info.IsDir() {
+		active = filepicker.New(state)
+	} else {
+		testcases, err := core.LoadTestCaseList(state)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		active = workspace.New(filepicker.FileSelectedMsg{Path: state, TestCases: testcases})
+	}
 
 	return Model{
-		active: filepicker.New(currentDirectory),
+		state:  state,
+		active: active,
 	}
 }
 
@@ -32,10 +56,12 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case error:
 		return m, tea.Quit
 	case filepicker.FileSelectedMsg:
+		m.state = msg.Path
 		m.active = workspace.New(msg)
 		return m, m.active.Init()
 	case workspace.SelectFileMsg:
-		m.active = filepicker.New(filepath.Dir(msg.PreviousFile))
+		m.state = filepath.Dir(msg.PreviousFile)
+		m.active = filepicker.New(m.state)
 		return m, m.active.Init()
 	default:
 		var cmd tea.Cmd
--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"log"
+	"os"
 
 	tea "charm.land/bubbletea/v2"
 	"github.com/thecomputerm/lazycph/internal/app"
 )
 
 func main() {
-	p := tea.NewProgram(app.New())
+	currentDirectory, _ := os.Getwd()
+	p := tea.NewProgram(app.New(currentDirectory))
 	if _, err := p.Run(); err != nil {
 		log.Fatalf("Alas, there's been an error: %v", err)
 	}
